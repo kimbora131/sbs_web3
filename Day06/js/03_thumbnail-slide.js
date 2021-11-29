5d3701050f06092b301a6193eccd4800268c8363
kimbora131 <kimbora131@gmail.com>
2021-11-29 22:28:01 +0900

    Day08

---
+++ Day06/js/03_thumbnail-slide.js
@@ -0,0 +1,30 @@
+let slideIndex = 1;
+
+$(function(){
+
+   
+
+    showSlides(slideIndex)
+})
+
+//슬라이드 함수
+function showSlides(n) {
+    let i;
+    let slides = $('.mySlides')
+    let dots = $('.demo')
+    let captionsText = $('#caption')
+
+    if( n > slides.length ){slideIndex = 1}
+    if( n < 1 ) {slideIndex = slides.length}
+    for(let i = 0 ; i < slides.length ; i++){
+        slides[i].css({display : 'none'})
+    }
+
+    for (let i = 0 ; i < dots.length ; i++){
+        dots[i].removeClass('active')
+    }
+    
+    slides[slideIndex-1].css({display : 'block'})
+    dots[slideIndex-1].addClass('active')
+    captionText.html( dots[slideIndex-1].alt )
+}
